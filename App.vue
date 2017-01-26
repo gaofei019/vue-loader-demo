b2b344818a5a9d1e060cea73f9cfe0e57d631480
gaofei019 <gaofei019@163.com>
2017-01-26 17:00:35 +0800

    add files

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>welcome Vue</h1>
+    <h2 @click="change">{{msg}}</h2>
+    <my-menu></my-menu>
+</template>
+<script>
+    import Menu from './components/Menu.vue'
+    export default{
+        data(){
+            return {
+                msg:'welcome vue'
+            }
+        },
+        methods:{
+            change(){
+                this.msg='wahaha'
+            }
+        },
+        components:{
+            'my-menu':Menu
+        }
+    }
+</script>
+<style>
+    body{
+        background:#ccc;
+    }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports={
+    entry:'./main.js',
+    output:{
+        path:__dirname,
+        filename:'bundle.js'
+    },
+    module:{
+        loaders:[
+            {test:/\.vue$/, loader:'vue'},
+            {test:/\.js$/,loader:'babel',exclude:/node_modules/}
+        ],
+    },
+    babel:{
+        presets:['es2015'],
+        plugins:['transform-runtime']
+    }
+};
